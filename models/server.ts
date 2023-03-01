0092036d371825b912e055aee6dbc05b54fc4aac
Luis Garcia <aaa@gamil.com>
2023-02-28 22:05:44 -0300

    Add middlewares

--- models/server.ts
+++ models/server.ts
@@ -1,18 +1,44 @@
 import express, { Application } from 'express';
+import userRouter from '../routes/usuario';
+import cors from 'cors';
 
 class Server{
 
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor(){
         this.app = express();
         this.port= process.env.PORT || '8000';
+
+        //Metodos iniciales
+        this.middlewares();
+        this.routes();
+
+    }
+
+    middlewares(){
+
+        //CORS
+        this.app.use( cors() );
+
+        //Lectura del body
+        this.app.use( express.json() );
+
+        //Carpeta publica
+        this.app.use( express.static('public'));
+    }
+
+    routes(){
+        this.app.use(this.apiPaths.usuarios, userRouter );
     }
 
     listen(){
         this.app.listen(this.port, ()=>{
-            console.log('Servidor corriendo en puerto ', this.port);
+            console.log('Servidor corriendo en puerto!: ', this.port);
         })
     }
 }
